5311a87e682d540f7ec5f4dc8535a86a05b82b35
ZoomOutt <stina.kalnina@gmail.com>
2019-11-12 11:35:24 +0200

    Commit Changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Blobex
+Display Json Data
---
+++ blobex-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BlobexList from './BlobexList'
+// import data from './blobex.json'
+
+export default function App() {
+  return (
+    <div>
+     <BlobexList/>
+    </div>
+  )
+}
+
