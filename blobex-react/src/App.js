3f8a237419d9ba43baef8e9b75815e40b7b2a5bf
ZoomOutt <stina.kalnina@gmail.com>
2019-11-12 11:52:41 +0200

    Prettier

--- blobex-react/src/App.js
+++ blobex-react/src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import BlobexList from './BlobexList'
 // import data from './blobex.json'
 
 export default function App() {
   return (
     <div>
-     <BlobexList/>
+      <BlobexList />
     </div>
   )
 }
-
