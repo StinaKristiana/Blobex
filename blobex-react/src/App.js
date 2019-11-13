30af9de40773c0705389391455ba33f69161eb04
ZoomOutt <stina.kalnina@gmail.com>
2019-11-13 08:32:35 +0200

    .

--- blobex-react/src/App.js
+++ blobex-react/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import BlobexList from './antd/BlobexList'
-
+import React from "react";
+import BlobexList from "./antd/BlobexList";
 
 export default function App() {
   return (
     <div>
       <BlobexList />
     </div>
-  )
+  );
 }
--- blobex-react/src/antd/BlobexList.js
+++ blobex-react/src/antd/BlobexList.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import data from '../blobex.json'
-import { Row, Col, Form, Input, Button, Select } from 'antd'
-import { DatePicker } from 'antd'
-import moment from 'moment'
+import React, { Component } from "react";
+import data from "../blobex.json";
+import { Row, Col, Form, Input, Button, Select } from "antd";
+import { DatePicker } from "antd";
+import moment from "moment";
 
-import 'antd/dist/antd.css'
-import './antd.css'
-import { node } from 'prop-types'
+import "antd/dist/antd.css";
+import "./antd.css";
+import { node } from "prop-types";
 
-const dateFormat = 'DD MMMM YYYY'
-const { Option } = Select
-const { Search } = Input
+const dateFormat = "DD MMMM YYYY";
+const { Option } = Select;
+const { Search } = Input;
 
 class BlobexList extends Component {
   onChange(value) {}
@@ -23,9 +23,9 @@ class BlobexList extends Component {
             return (
               <Row gutter={[50, 5]}>
                 <Col span={24} className="form-header">
-                  <Button style={{ border: 0, color: 'salmon' }}>Cancel</Button>
+                  <Button style={{ border: 0, color: "salmon" }}>Cancel</Button>
                   <h1>{widgets.name}</h1>
-                  <Button style={{ border: 0, color: 'green' }}>Save</Button>
+                  <Button style={{ border: 0, color: "green" }}>Save</Button>
                 </Col>
 
                 {widgets.items.map(details => {
@@ -45,21 +45,21 @@ class BlobexList extends Component {
                               <Input
                                 style={
                                   obj.required
-                                    ? { borderLeft: '0.2rem solid green' }
+                                    ? { borderLeft: "0.2rem solid green" }
                                     : {}
                                 }
                                 className="form-placeholder"
                                 type={
-                                  obj.type === 'currency' ? 'number' : 'text'
+                                  obj.type === "currency" ? "number" : "text"
                                 }
                                 placeholder={
-                                  obj.symbol === 'EUR'
+                                  obj.symbol === "EUR"
                                     ? `€ ${obj.value}`
                                     : `${obj.value}`
                                 }
                               />
                             </Form.Item>
-                          )
+                          );
                         })}
                       </Col>
                       <Col span={12}>
@@ -71,8 +71,8 @@ class BlobexList extends Component {
                           <Select
                             showSearch
                             style={{
-                              width: '100%',
-                              borderLeft: '0.2rem solid green',
+                              width: "100%",
+                              borderLeft: "0.2rem solid green"
                             }}
                             placeholder="Proposal/Price Quote"
                             optionFilterProp="children"
@@ -83,13 +83,13 @@ class BlobexList extends Component {
                                 .indexOf(input.toLowerCase()) >= 0
                             }
                           >
-                            <Option value="10%" style={{ color: 'salmon' }}>
+                            <Option value="10%" style={{ color: "salmon" }}>
                               10%
                             </Option>
-                            <Option value="25%" style={{ color: 'grey' }}>
+                            <Option value="25%" style={{ color: "grey" }}>
                               25%
                             </Option>
-                            <Option value="50%" style={{ color: 'black' }}>
+                            <Option value="50%" style={{ color: "black" }}>
                               50%
                             </Option>
                           </Select>
@@ -125,14 +125,14 @@ class BlobexList extends Component {
                         </Form.Item>
                       </Col>
                     </Col>
-                  )
+                  );
                 })}
               </Row>
-            )
+            );
           })}
         </div>
       </div>
-    )
+    );
   }
 }
-export default BlobexList
+export default BlobexList;
